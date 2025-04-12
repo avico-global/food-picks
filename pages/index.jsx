c58f0d375c39cc7ed38cbd34ed4219dc3e4ec739
Usama <usamamustafa393@gmail.com>
2025-04-12 11:12:13 +0500

    push

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,11 @@ import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
     <div>
+      {/* Navbar */}
       <Navbar />
+      {/* Hero Section */}
+      {/* Testimonials */}
+      {/* Footer */}
     </div>
   );
 }
